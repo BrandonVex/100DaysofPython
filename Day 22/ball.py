5b2ff91cac355c0f4c5c390c56a1b96b1f912219
Brandon <BrandonKuffWork@gmail.com>
2024-08-10 11:18:48 -0700

    Update ball.py

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -1,4 +1,11 @@
 from turtle import Turtle
 
-class Paddle(Turtle):
+class Ball(Turtle):
     
+    def __init__(self, position):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(position)
+        
