3f0ce5da6b58ccf40d0c7861c5f2d19a06d61cb9
Brandon <BrandonKuffWork@gmail.com>
2024-08-12 19:00:15 -0700

    Day 22 Completion

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -2,10 +2,48 @@ from turtle import Turtle
 
 class Ball(Turtle):
     
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.goto(position)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1  # Initialize move_speed here
+        
+    # function for moving the ball x and y
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+        
+    # function for bouncing the ball
+    def bounce_y(self):
+        self.y_move *= -1
+        
+    # paddle bounce
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9  # Correct the typo here to actually update the move_speed
+        
+    # reset ball
+    def reset_ball(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1  # Reset move_speed when the ball is reset
+        self.bounce_x()
+
+# ball = Ball()
+# ball.move()
+# ball.bounce()
+# ball.paddle_bounce()
+# ball.reset_ball()
+# print(ball.xcor())
+        
+        
+        
+    
+    
+        
+
+
         
--- Day 22/main.py
+++ Day 22/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 # screen
 screen = Screen()
@@ -12,6 +15,12 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+# create ball for pong
+ball = Ball()
+
+# scoreboard
+scoreboard = Scoreboard()
+
 # move paddles
 screen.listen()
 screen.onkey(r_paddle.paddle_up, "Up")   # Right paddle with Up arrow key
@@ -23,9 +32,28 @@ screen.onkey(l_paddle.paddle_down, "s") # Left paddle with 's' key
 # game on
 game_on = True
 while game_on:
+    time.sleep(ball.move_speed)
     screen.update()
-
+    ball.move()
+    
+    # detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+        
+    # detect when the ball goes out of bounds on the right side
+    if ball.xcor() > 400:
+        ball.reset_ball()
+        scoreboard.l_point()
+        
+    # detect when the ball goes out of bounds on the left side
+    if ball.xcor() < -400:
+        ball.reset_ball()
+        scoreboard.r_point()
+        
 # screen onclick
 screen.exitonclick()
 
-# AFK < -----------------------
