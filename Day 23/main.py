22bc5b83c82a4c484267ed90bea201e7e2569d7e
Brandon <BrandonKuffWork@gmail.com>
2024-08-30 12:34:03 -0700

    updates

--- Day 23/main.py
+++ Day 23/main.py
@@ -6,9 +6,23 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("white")
+screen.title("Crossy Cars")
 screen.tracer(0)
+
+# create player
+player = Player()
+# create car manager
+car_manager = CarManager()
+
+# screen listen
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_cars()
