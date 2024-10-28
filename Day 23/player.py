9a487c415d748353a54b64dd332156de06f68899
Brandon <BrandonKuffWork@gmail.com>
2024-10-28 04:41:27 -0700

    Finished Project

--- Day 23/player.py
+++ Day 23/player.py
@@ -10,10 +10,20 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)  # Make sure the turtle is facing up
     
     def go_up(self):
         self.forward(MOVE_DISTANCE)
 
-
+    def go_down(self):
+        self.backward(MOVE_DISTANCE)
+        
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+        
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
