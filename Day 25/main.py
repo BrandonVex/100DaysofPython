ab41ba2b401a64b6e0faa89256c818978ff21822
Brandon <BrandonKuffWork@gmail.com>
2024-11-09 18:20:57 -0700

    update

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,4 +1,5 @@
 import csv
+import pandas
 
 # Open the CSV file
 with open("weather_data.csv") as data_file:
@@ -9,3 +10,8 @@ with open("weather_data.csv") as data_file:
             temperatures.append(int(row[1]))
     print(temperatures)
     
+# Using the pandas library
+pandas.read_csv("weather_data.csv") # This is the same as the code above
+print(data) # This will print the data in a table format
+
+print(data["temp"]) # This will print the temperature column
