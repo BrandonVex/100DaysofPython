ce2d6fcc1de3c1180ff3a090c69669361ee040dc
Brandon <BrandonKuffWork@gmail.com>
2024-11-09 18:27:56 -0700

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -15,3 +15,18 @@ pandas.read_csv("weather_data.csv") # This is the same as the code above
 print(data) # This will print the data in a table format
 
 print(data["temp"]) # This will print the temperature column
+
+# series is equivalent to a column in a table
+data_dict = data.to_dict() # This will convert the data to a dictionary
+print(data_dict)
+
+# Convert the temperature column to a list
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+# average sum of the temperature
+average_temp = data["temp"].mean()
+print(average_temp)
+
+
+
