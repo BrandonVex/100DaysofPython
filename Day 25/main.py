41c4512874170ae89769618a71ee290630dcb6fa
Brandon <BrandonKuffWork@gmail.com>
2024-12-02 12:13:45 -0700

    Learning Pandas

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,32 +1,57 @@
-import csv
 import pandas
 
-# Open the CSV file
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
-    
-# Using the pandas library
-pandas.read_csv("weather_data.csv") # This is the same as the code above
-print(data) # This will print the data in a table format
+data = pandas.read_csv("Day 25/weather_data.csv")
+print(data)
 
-print(data["temp"]) # This will print the temperature column
-
-# series is equivalent to a column in a table
-data_dict = data.to_dict() # This will convert the data to a dictionary
+# this converts the data into a dictionary for you can use it in a different way
+data_dict = data.to_dict()
 print(data_dict)
 
-# Convert the temperature column to a list
+# temperature column is a series converted to a list
 temp_list = data["temp"].to_list()
 print(temp_list)
 
-# average sum of the temperature
+# average temperature
 average_temp = data["temp"].mean()
 print(average_temp)
 
+# maximum temperature
+max_temp = data["temp"].max()
+print(max_temp)
 
+# get data in column by index - condition is the column name
+# they have to match identically with the column name in csv
+print(data["condition"])
+# data.condition works the same as stated above
+print(data.condition)
 
+# get an entire data row inside a row by index
+# monday is the row we want inside the day index
+print(data[data.day == "Monday"])
+# this has a condition of the highest temperature
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+# convert Monday temperature to Fahrenheit
+# we have to get the temperature value first by indexing it at 0 without the index the code will return a series
+# then use the correct formula to convert it to Fahrenheit
+monday_temp = monday.temp[0]
+monday_temp_f = monday_temp * 9/5 + 32
+print(monday_temp_f)
+
+# create dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65],
+    "grades": ["A", "B", "A"],
+    "height": [1.65, 1.75, 1.80],
+    "weight": [55, 70, 65],
+}
+
+# this will create a dataframe from the data_dict class above
+data = pandas.DataFrame(data_dict)
+
+# save to csv into a path
+data.to_csv("Day 25/new_data.csv")
