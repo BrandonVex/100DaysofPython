7bc8b62e3d67ab1fb5c0ebf938b863125cbd8850
Brandon <BrandonKuffWork@gmail.com>
2025-02-18 18:21:19 -0700

    Update main.py

--- Day 26/main.py
+++ Day 26/main.py
@@ -23,5 +23,5 @@ for (index, row) in student_data_frame.iterrows():
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs. s
 
